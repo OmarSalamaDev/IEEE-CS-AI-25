b55b677c39ec19019965d3ac172d07af26b6e71b
Omar Salama <omar550205@gmail.com>
2025-02-20 00:17:00 +0200

    forgot some thing ._.

--- Task_4/problems.py
+++ Task_4/problems.py
@@ -1,10 +1,12 @@
 
+
 # that takes list from user
 def get_numbers():
     nums = list(map(int, input().split(", ")))
     nums.sort()
     print(nums)
     return nums
+
 
 # returns min
 def find_min(numbers):
@@ -14,6 +16,7 @@ def find_min(numbers):
             min_num = num
     return min_num
 
+
 # returns max 
 def find_max(numbers):
     max_num = numbers[0]
@@ -22,28 +25,32 @@ def find_max(numbers):
             max_num = num
     return max_num
 
+
 # returns mean
 def find_mean(numbers):
     num_sum = 0
     for num in numbers:
         num_sum += num
     return num_sum / len(numbers)
-     
+
+
 # returns mode
 def find_mode(numbers):
     occurences = {}
+    mode = []
     for num in numbers:
         if num in occurences:
             occurences[num] += 1
         else:
             occurences[num] = 1
     max_occurences = 0
-    max_num = 0
     for num in occurences:
         if occurences[num] > max_occurences:
+            mode = [num]
             max_occurences = occurences[num]
-            max_num = num
-    return max_num
+        elif occurences[num] == max_occurences:
+            mode.append(num)
+    return mode
 
 
 # returns median 
@@ -54,45 +61,44 @@ def find_median(numbers):
     else:
         return numbers[len(numbers) // 2]
     
+
 # Returns range
 def find_range(numbers):
     return max(numbers) - min(numbers)
+
 
 # Returns variance
 def find_variance(numbers):
     mean = sum(numbers) / len(numbers)
     return sum((x - mean) ** 2 for x in numbers) / len(numbers)
 
+
 # Returns standard deviation
 def find_STD(numbers):
     return find_variance(numbers) ** 0.5
 
+
 # Returns Q1, Q2, Q3
 def find_Quartiles(numbers):
-    sorted_nums = sorted(numbers)
-    n = len(sorted_nums)
+    
+    n = len(numbers)
 
-    # Median (Q2)
-    if n % 2 == 0:
-        Q2 = (sorted_nums[n//2 - 1] + sorted_nums[n//2]) / 2
-    else:
-        Q2 = sorted_nums[n//2]
+    Q2 = find_median(numbers)
 
-    # First quartile (Q1)
-    lower_half = sorted_nums[:n//2]
+    lower_half = numbers[:n//2]
     if len(lower_half) % 2 == 0:
         Q1 = (lower_half[len(lower_half)//2 - 1] + lower_half[len(lower_half)//2]) / 2
     else:
         Q1 = lower_half[len(lower_half)//2]
 
-    # Third quartile (Q3)
-    upper_half = sorted_nums[(n+1)//2:]
+    upper_half = numbers[(n+1)//2:]
     if len(upper_half) % 2 == 0:
         Q3 = (upper_half[len(upper_half)//2 - 1] + upper_half[len(upper_half)//2]) / 2
     else:
         Q3 = upper_half[len(upper_half)//2]
 
     return Q1, Q2, Q3
+
 
 # Returns Interquartile range
 def find_IQR(numbers):
